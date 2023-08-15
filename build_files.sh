ffd773dca78935ce38e8e1774db34b772b7cd072
rajasekhar <raja1102nr@gmail.com>
2023-08-15 14:15:36 +0530

    commit-4

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-sudo pip3 install virtaulenv
-virtaulenv newenv
+sudo pip3 install virtualenv
+virtualenv newenv
 source newenv/bin/activate
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
